91cd7ccea9e4bce805901ee49ac4a4434bfc94d6
berkecelikdev <berkecelik1221@gmail.com>
2026-04-06 00:49:52 +0300

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,14 +1,17 @@
 class Node<T>{
 
-    T data;
-    Node<T> next;
+    T data;  // Verinin kendisi
+    Node<T> next;   // bir sonraki Node objesinin bellekteki adresini tutar
 
+    // sadece veri verilip "sonraki düğüm" belirtilmediğinde çalışır
     Node(T dataPortion){
         this(dataPortion, null);
     }
 
+    // Ana constructor
     Node(T dataPortion, Node<T> nextNode){
-        data = dataPortion;
-        next = nextNode;
+        data = dataPortion;   // Gelen veriyi düğümün içine yerleştirir
+        next = nextNode;   // Düğümün kendisinden sonra hangi düğüme bağlanacağını belirler
     }
 }
+
